f3e131c841b1c969d70766e6fbad720641bcf8b4
vaniaazalia <vania.putri7780@smk.belajar.id>
2025-10-27 09:25:29 +0700

    Generate Model, Migration, Control lerand Table Desaign

---
+++ database/migrations/2025_10_27_014301_create_transactions_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id();
+            $table->foreign('user_id')->constrained();
+            $table->date('date');
+            $table->integer('total');
+            $table->integer('pay_total');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('transactions');
+    }
+};
